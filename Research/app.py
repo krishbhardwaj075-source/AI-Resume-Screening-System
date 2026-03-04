6351a4bed07287c5bc04f77b3d0a041c64bc47ba
krishbhardwaj075-source <your_github_email@example.com>
2026-03-05 05:11:50 +0530

    update

--- Research/app.py
+++ Research/app.py
@@ -57,4 +57,4 @@ def dashboard():
     resumes = all_resumes()
     return render_template("dashboard.html", resumes=resumes)
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
